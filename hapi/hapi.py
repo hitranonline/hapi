a05579f91aa22bdf696110b644d6573ebca93247
Josh Soref <2119212+jsoref@users.noreply.github.com>
2023-08-06 02:21:08 -0400

    spelling: should
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- hapi/hapi.py
+++ hapi/hapi.py
@@ -4287,7 +4287,7 @@ GammaL:    (optional parameter)
 
 HITRAN_units:    (optional parameter)
 
-  Logical flag for units, in which the absorption coefficient shoould be 
+  Logical flag for units, in which the absorption coefficient should be 
   calculated. Currently, the choices are: cm^2/molec (if True) and
   cm-1 (if False).
   NOTE: to calculate other spectral functions like transmitance,
